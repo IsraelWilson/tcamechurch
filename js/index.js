ceb6b3d4e6269865f1c186bf63d5ea313940d61d
Israel Wilson <israelsjwilson@gmail.com>
2018-08-20 22:43:50 -0500

    Added user validation. Check if user has already voted. Check for admin password.

--- js/index.js
+++ js/index.js
@@ -1,22 +1,25 @@
 $(document).ready(function(){
   $("#btn-signin").click(function(){
     var id = $("#inputID").val();
-
-    if(id){
-      $.ajax({
-        url: "php/login.php",
-        type: "POST",
-        data: {
-          userID: id
-        },
-        success: function(data){
-          signIn(data);
-        },
-        dataType: "text"
-      });
-    }
-    else{
-      $("#signin-err").show();
+    if(id == "tccadmin"){
+      location.href = "dashboard.html";
+    }else{
+      if(id){
+        $.ajax({
+          url: "php/login.php",
+          type: "POST",
+          data: {
+            userID: id
+          },
+          success: function(data){
+            signIn(data);
+          },
+          dataType: "text"
+        });
+      }
+      else{
+        $("#signin-err").show();
+      }
     }
 
   });
@@ -28,6 +31,9 @@ $(document).ready(function(){
       $("#signin-err").text("voting has been closed");
       $("#signin-err").show();
     }else if (data == 2) {
+      $("#signin-err").text("user has already voted");
+      $("#signin-err").show();
+    }else if (data == 3) {
       $("#signin-err").text("user does not exist");
       $("#signin-err").show();
     }else{
--- php/login.php
+++ php/login.php
@@ -3,8 +3,8 @@ session_start();
 /* This file is run whenever the login button is clicked from index.html
  * If voting has been closed exit returns an error string to be displayed
  * for the user on index.html. If voting is open and a user is found
- * print 1. Exit with error code 0, 2 if voting is closed or
- * user does not exist respectively.
+ * print 1. Exit with error code 0, 2, 3 if voting is closed, user already
+ * voted, or user does not exist respectively.
 */
 
   if(isset($_POST["userID"])){
@@ -21,10 +21,14 @@ session_start();
       if($row[0]){
         $user = mysqli_query($con, "SELECT * FROM user WHERE user_id='$userID'");
         if(mysqli_num_rows($user) > 0){
-          $_SESSION["user"] = $userID;
-          print 1;
+          if(!mysqli_fetch_row($user)[1]){
+            $_SESSION["user"] = $userID;
+            print 1;
+          }else{
+            print 2;
+          }
         }else{
-          print 2;
+          print 3;
         }
       }else{
         print 0;
